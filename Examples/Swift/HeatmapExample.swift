deb1923f36687dac1ce2c31cdbba9f12ad0b182e
jmkiley <jordan.kiley@mapbox.com>
2018-05-24 13:54:33 -0700

    Testing out querying workaround

--- Examples/Swift/HeatmapExample.swift
+++ Examples/Swift/HeatmapExample.swift
@@ -4,26 +4,35 @@ import Mapbox
 @objc(HeatmapExample_Swift)
 
 class HeatmapExample: UIViewController, MGLMapViewDelegate {
-
+    
+    var mapView : MGLMapView!
+    var source : MGLShapeSource!
+    var heatmapLayer : MGLHeatmapStyleLayer!
+    var circleLayer : MGLCircleStyleLayer!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Create and add a map view.
-        let mapView = MGLMapView(frame: view.bounds, styleURL: MGLStyle.lightStyleURL)
+        mapView = MGLMapView(frame: view.bounds, styleURL: MGLStyle.lightStyleURL)
         mapView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
         mapView.delegate = self
         mapView.tintColor = .lightGray
         view.addSubview(mapView)
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(tap:)))
+        mapView.addGestureRecognizer(tap)
     }
     
     func mapView(_ mapView: MGLMapView, didFinishLoading style: MGLStyle) {
         // Parse GeoJSON data. This example uses all M1.0+ earthquakes from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
-        guard let url = URL(string: "https://www.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson") else { return }
-        let source = MGLShapeSource(identifier: "earthquakes", url: url, options: nil)
+        
+        // ~ 1200 points
+        guard let url = URL(string: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_month.geojson") else { return }
+        source = MGLShapeSource(identifier: "earthquakes", url: url, options: nil)
         style.addSource(source)
         
         // Create a heatmap layer.
-        let heatmapLayer = MGLHeatmapStyleLayer(identifier: "earthquakes", source: source)
+        heatmapLayer = MGLHeatmapStyleLayer(identifier: "earthquakes", source: source)
         
         // Adjust the color of the heatmap based on the point density.
         let colorDictionary : [NSNumber : UIColor] = [
@@ -53,18 +62,32 @@ class HeatmapExample: UIViewController, MGLMapViewDelegate {
         style.addLayer(heatmapLayer)
         
         // Add a circle layer to represent the earthquakes at higher zoom levels.
-        let circleLayer = MGLCircleStyleLayer(identifier: "circle-layer", source: source)
-        
+        circleLayer = MGLCircleStyleLayer(identifier: "circle-layer", source: source)
+
         let magnitudeDictionary : [NSNumber : UIColor] = [0 : .white,
                                                           0.5 : .yellow,
                                                           2.5 : UIColor(red:0.73, green:0.23, blue:0.25, alpha:1.0),
                                                           5 : UIColor(red:0.19, green:0.30, blue:0.80, alpha:1.0)
         ]
         circleLayer.circleColor = NSExpression(format: "mgl_interpolate:withCurveType:parameters:stops:(mag, 'linear', nil, %@)", magnitudeDictionary)
-        
+
         // The heatmap layer will have an opacity of 0.75 up to zoom level 9, when the opacity becomes 0.
-        circleLayer.circleOpacity = NSExpression(format: "mgl_step:from:stops:($zoomLevel, 0, %@)", [0: 0, 9: 0.75])
-        circleLayer.circleRadius = NSExpression(forConstantValue: 20)
+//        circleLayer.circleOpacity = NSExpression(format: "mgl_step:from:stops:($zoomLevel, 0, %@)", [0: 0, 9: 0.75])
+        circleLayer.circleOpacity = NSExpression(forConstantValue: 0)
+//        circleLayer.circleRadius = NSExpression(forConstantValue: 20)
         style.addLayer(circleLayer)
+        
+
+    }
+    
+    @objc func handleTap(tap: UITapGestureRecognizer) {
+        let point = tap.location(in: mapView)
+        let rect = CGRect(x: point.x - 100, y: point.y - 100, width: point.x + 200, height: point.y + 200)
+        let features = mapView.visibleFeatures(in: rect, styleLayerIdentifiers: ["earthquakes", "circle-layer"])
+//        print(features)
+        let ids = features.map { $0.attribute(forKey: "ids") }
+//        heatmapLayer.predicate = NSPredicate(format: "%K IN %@", "ids", ids)
+        
+        
     }
 }
